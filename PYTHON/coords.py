d0296b3dbf368fd68f295344f50cedb74b821f00
hari632 <harinidars@gmail.com>
2026-05-30 07:43:45 +0530

    PYTHON PROJECT

---
+++ PYTHON/coords.py
@@ -0,0 +1,10 @@
+def display(coords):
+    if(len(coords)!=2):
+        print("Invalid Coordinates")
+        return
+    x,y=coords
+    print(f"X:{x}")
+    print(f"Y:{y}")
+coordinates=(10,15)
+display(coordinates)
+    
