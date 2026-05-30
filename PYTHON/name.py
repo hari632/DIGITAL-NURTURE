ae4b93a3376deeea7b3cb1e6d0a2ee602c974128
hari632 <harinidars@gmail.com>
2026-05-30 08:22:03 +0530

    GREET NAME

---
+++ PYTHON/name.py
@@ -0,0 +1,8 @@
+def greetname(name):
+    if len(name)==0:
+        print("invalid input")
+        return
+    print(f"{name} Welcome to the DIGITAL NURTURE")
+name=str(input("Enter the name: "))
+greetname(name)
+    
