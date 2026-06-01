9147725aa0d671ac881cc8a6169e79c0c96fa616
hari632 <harinidars@gmail.com>
2026-06-01 19:41:19 +0530

    PYTHON PROJECT

---
+++ PYTHON/sqrt.py
@@ -0,0 +1,12 @@
+from math import *
+def  fuction(x):
+    if x<0:
+        print("INVALID NUMBER")
+        return
+    square=sqrt(x)
+    power=pow(x,2)
+    print(f"square root:{square:.2f}")
+    print(f"power of two:{power}")
+    print(f"value of pi: {pi:.2f}")
+fuction(34)    
+    
