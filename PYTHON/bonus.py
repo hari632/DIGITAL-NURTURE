acc7f230c4529bb8b45403323cf2fc7dc21f5ff9
hari632 <harinidars@gmail.com>
2026-06-01 18:57:54 +0530

    PYTHON PROJECT

---
+++ PYTHON/bonus.py
@@ -0,0 +1,8 @@
+def basebonus():
+    # base amount of the salary
+    base =int(input("Enter the base salary amount"))
+    # bonus amount of the salary
+    bonus=int(input("Enter the bonus salary amount"))
+    ts=base+bonus
+    print(f"TOTAL SALARY:{ts}")
+basebonus()    
